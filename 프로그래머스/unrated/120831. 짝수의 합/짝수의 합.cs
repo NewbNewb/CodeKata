c986fdf99b73e7e191669750c33da71f26fab787
newb <tydeo6029@naver.com>
2023-11-21 10:03:39 +0900

    [level unrated] Title: 짝수의 합, Time: 0.18 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if(n % 2 != 0)
+        {
+            n--;
+        }
+        while(n >= 0)
+        {
+            answer += n;
+            n -= 2;
+        }
+        return answer;
+    }
+}
