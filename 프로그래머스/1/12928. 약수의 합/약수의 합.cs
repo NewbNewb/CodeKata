88e8471673369cbef9a00e3ede5b946cccc4b50f
newb <tydeo6029@naver.com>
2023-11-22 09:33:50 +0900

    [level 1] Title: 약수의 합, Time: 0.17 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.cs
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cs
@@ -3,10 +3,9 @@ public class Solution {
         int answer = 0;
         for (int i = 1; i <= n; i++)
         {
-            answer += i;
-            if (n % i != 0)
+            if (n % i == 0)
             {
-                answer -= i;
+                answer += i;
             }
         }
         return answer;
