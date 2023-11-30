b0ebd3018861978e9a72b367ee069e57f67c43e5
newb <tydeo6029@naver.com>
2023-11-30 13:50:57 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.17 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cs
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cs
@@ -1,22 +1,19 @@
-using System;
-
 public class Solution {
-    public int solution(int num)
+    public int solution(long num)
     {
         int count = 0;
-        long num1 = num;
 
         for (; count < 500; count++)
         {
-            if (num1 != 1)
+            if (num != 1)
             {
-                if (num1 % 2 == 0)
+                if (num % 2 == 0)
                 {
-                    num1 /= 2;
+                    num /= 2;
                 }
                 else
                 {
-                    num1 = num1 * 3 + 1;
+                    num = num * 3 + 1;
                 }
             }
             else
