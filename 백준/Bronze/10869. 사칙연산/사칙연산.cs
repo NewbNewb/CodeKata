5d22b808d515df2a1ec3c74911fb5fbd415b8931
newb <tydeo6029@naver.com>
2023-12-29 20:44:41 +0900

    [Bronze V] Title: 사칙연산, Time: 64 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,16 @@
+public class Example
+{
+    public static void Main()
+    {
+        string[] s = Console.ReadLine().Split(' ');
+
+        int a = int.Parse(s[0]);
+        int b = int.Parse(s[1]);
+
+        Console.WriteLine(a + b);
+        Console.WriteLine(a - b);
+        Console.WriteLine(a * b);
+        Console.WriteLine(a / b);
+        Console.WriteLine(a % b);
+    }
+}
