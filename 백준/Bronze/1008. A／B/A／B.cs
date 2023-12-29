6a1516dd113659023543b53d5c1081b736a8306f
newb <tydeo6029@naver.com>
2023-12-29 20:58:50 +0900

    [Bronze V] Title: A/B, Time: 64 ms, Memory: 5508 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.cs
+++ 백준/Bronze/1008. A／B/A／B.cs
@@ -4,8 +4,8 @@ public class Example
     {
         string[] s = Console.ReadLine().Split(' ');
 
-        Double a = int.Parse(s[0]);
-        Double b = int.Parse(s[1]);
+        double a = int.Parse(s[0]);
+        double b = int.Parse(s[1]);
 
         Console.WriteLine(a / b);
     }
