6054ea72460c8c7298a4c9549d476c96c8ee9f44
newb <tydeo6029@naver.com>
2024-07-02 16:50:59 +0900

    [Bronze III] Title: 오븐 시계, Time: 72 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string[] ss = Console.ReadLine().Split(' ');
+        int.TryParse(ss[0], out int n1);
+        int.TryParse(ss[1], out int n2);
+        int.TryParse(Console.ReadLine(), out int n3);
+
+        n2 += n3;
+
+        while (n2 > 59)
+        {
+            n1++;
+            n2 -= 60;
+        }
+        if (n1 > 23)
+        {
+            n1 -= 24;
+        }
+        Console.WriteLine(n1 + " " + n2);
+    }
+}
